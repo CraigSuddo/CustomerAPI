95e5e7c7c55b89cb9b8f26ceddf18af072e3661e
Craig Suddo <craigsuddo@gmail.com>
2021-08-19 11:20:44 +0100

    Setting up the two specified entities.

--- CustomerAPI.Entities/Address.cs
+++ CustomerAPI.Entities/Address.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,32 @@ namespace CustomerAPI.Entities
 {
     public class Address
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+        public string CustomerId { get; set; }
+
+        [MaxLength(80)]
+        [Required]
+        public string AddressLine1 { get; set; }
+
+        [MaxLength(80)]
+        public string AddressLine2 { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Town { get; set; }
+
+        [MaxLength(50)]
+        public string County { get; set; }
+
+        [MaxLength(10)]
+        [Required]
+        public string Postcode { get; set; }
+
+        public string Country { get; set; }
     }
 }
--- CustomerAPI.Entities/Customer.cs
+++ CustomerAPI.Entities/Customer.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CustomerAPI.Entities
 {
     public class Customer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Forename { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Surname { get; set; }
+
+        [MaxLength(75)]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [MaxLength(15)]
+        [Required]
+        public string MobileNo { get; set; }
+
+        public List<Address> Addresses { get; set; }
     }
 }
