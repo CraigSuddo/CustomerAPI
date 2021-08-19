af3ff4a0ff02990c0ca3df761e2428abb38ed1f4
Craig Suddo <craigsuddo@gmail.com>
2021-08-19 11:54:04 +0100

    Adding place holder for controllers.

---
+++ CustomerAPI/Controllers/AddressController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddressController : ControllerBase
+    { 
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
